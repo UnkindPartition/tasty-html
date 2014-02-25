046aa623b8e918b732e7551ccc164bfab6139029
Danny Navarro <j@dannynavarro.net>
2014-02-25 10:14:08 +0100

    First attempt to render HTML instead of XML
    It typechecks but the example output only shows a failed summary.
    The attributes and tag elements will probably have to be modified to
    match bootstrap-tree.
    
    See #1.

--- Test/Tasty/Runners/Html.hs
+++ Test/Tasty/Runners/Html.hs
@@ -8,10 +8,9 @@
 module Test.Tasty.Runners.Html (htmlRunner) where
 
 import Control.Applicative
-import Control.Arrow (first)
 import Control.Monad.Trans.Class (lift)
 import Data.Maybe (fromMaybe)
-import Data.Monoid (Monoid(..), Endo(..), Sum(..))
+import Data.Monoid (Monoid(..), Sum(..))
 import Data.Proxy (Proxy(..))
 import Data.Semigroup.Applicative (Traversal(..))
 import Data.Tagged (Tagged(..))
@@ -25,7 +24,8 @@ import qualified Data.Functor.Compose as Functor
 import qualified Data.IntMap as IntMap
 import qualified Test.Tasty.Options as Tasty
 import qualified Test.Tasty.Runners as Tasty
-import qualified Text.XML.Light as XML
+-- import qualified Text.XML.Light as XML
+import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as HA
 import Text.Blaze.Html.Renderer.String (renderHtml)
@@ -44,7 +44,7 @@ instance Tasty.IsOption (Maybe HtmlPath) where
 data Summary = Summary { summaryFailures :: Sum Int
                        , summaryErrors :: Sum Int
                        , summarySuccesses :: Sum Int
-                       , xmlRenderer :: Endo XML.Element
+                       , htmlRenderer :: H.Markup
                        } deriving (Generic)
 
 instance Monoid Summary where
@@ -69,75 +69,67 @@ htmlRunner = Tasty.TestReporter optionDescription runner
     HtmlPath path <- Tasty.lookupOption options
 
     return $ \statusMap ->
-      let
 
+      let
         runTest _ testName _ = Traversal $ Functor.Compose $ do
           i <- State.get
 
           summary <- lift $ STM.atomically $ do
             status <- STM.readTVar $
               fromMaybe (error "Attempted to lookup test by index outside bounds") $
-                IntMap.lookup i statusMap
+              IntMap.lookup i statusMap
 
-            let testCaseAttributes = map (uncurry XML.Attr . first XML.unqual)
-                  [ ("name", testName ) ]
+            let testCaseContent = H.toMarkup testName
 
                 mkSummary contents =
-                  mempty { xmlRenderer = Endo
-                             (`appendChild` XML.node (XML.unqual "testcase") contents)
+                  mempty { htmlRenderer =
+                            ((H.li ! HA.class_ "testcase" $ contents))
                          }
 
-                mkSuccess = (mkSummary testCaseAttributes) { summarySuccesses = Sum 1 }
+                mkSuccess = (mkSummary testCaseContent) { summarySuccesses = Sum 1 }
 
                 mkFailure reason =
-                  mkSummary ( testCaseAttributes
-                            , XML.node (XML.unqual "failure") reason
-                            )
+                  mkSummary (H.li ! HA.class_ "failure" $ H.toMarkup reason)
 
             case status of
-              -- If the test is done, generate XML for it
+              -- If the test is done, generate HTML for it
               Tasty.Done result
                 | Tasty.resultSuccessful result -> pure mkSuccess
                 | otherwise -> pure $
-                    (mkFailure (Tasty.resultDescription result))
+                    (mkFailure $ Tasty.resultDescription result)
                       { summaryFailures = Sum 1 }
-
               -- Otherwise the test has either not been started or is currently
               -- executing
               _ -> STM.retry
 
-          Const summary <$ State.modify (+ 1)
+          Const summary <$ State.modify (+1)
 
         runGroup groupName children = Traversal $ Functor.Compose $ do
           Const soFar <- Functor.getCompose $ getTraversal children
-          let grouped = appEndo (xmlRenderer soFar) $
-                XML.node (XML.unqual "testsuite") $
-                  XML.Attr (XML.unqual "name") groupName
+          let grouped = (htmlRenderer soFar) `mappend`
+                (H.ul ! HA.class_ "testsuite" $ H.toMarkup groupName)
 
           pure $ Const
-            soFar { xmlRenderer = Endo (`appendChild` grouped)
+            soFar { htmlRenderer = grouped
                   }
 
       in do
         (Const summary, tests) <-
           flip State.runStateT 0 $ Functor.getCompose $ getTraversal $
-           Tasty.foldTestTree
-             Tasty.trivialFold { Tasty.foldSingle = runTest, Tasty.foldGroup = runGroup }
-             options
-             testTree
+          Tasty.foldTestTree
+            Tasty.trivialFold { Tasty.foldSingle = runTest, Tasty.foldGroup = runGroup }
+            options
+            testTree
 
         writeFile path $
-         renderHtml $
-          H.docTypeHtml $ do
-            H.head $ do
-              H.title "Test Results"
-            H.body $ do
-              H.p "summaryErrors"
-              H.p H.! HA.class_ "summaryErrors" $ H.toHtml $ (show . getSum . summaryErrors $ summary)
-              H.p H.! HA.class_ "summaryFailures" $ H.toHtml (show . getSum . summaryFailures $ summary)
-                --, XML.Attr (XML.unqual "tests") (show tests)
+          renderHtml $
+            H.docTypeHtml $ do
+              H.head $ do
+                H.title "Test Results"
+              H.body $ do
+                H.p "summaryErrors"
+                H.p H.! HA.class_ "summaryErrors" $ H.toHtml $ (show . getSum . summaryErrors $ summary)
+                H.p H.! HA.class_ "summaryFailures" $ H.toHtml (show . getSum . summaryFailures $ summary)
+                 --, XML.Attr (XML.unqual "tests") (show tests)
 
         return (getSum ((summaryFailures `mappend` summaryErrors) summary) == 0)
-
-  appendChild parent child =
-    parent { XML.elContent = XML.elContent parent ++ [ XML.Elem child ] }
