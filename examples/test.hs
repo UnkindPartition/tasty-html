524e453256b86e70125ca89bef5289edfc712bce
Danny Navarro <j@dannynavarro.net>
2014-02-27 09:54:26 +0100

    Extend example test

--- examples/test.hs
+++ examples/test.hs
@@ -1,14 +1,41 @@
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.SmallCheck as SC
+import Test.Tasty.QuickCheck as QC
 import Test.Tasty.Runners.Html
 
 import Data.List
 import Data.Ord
 
-main = defaultMainWithIngredients [htmlRunner] tests
+main = defaultMainWithIngredients (htmlRunner:defaultIngredients) tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Tests" [properties, unitTests]
+
+properties :: TestTree
+properties = testGroup "Properties" [scProps, qcProps]
+
+scProps = testGroup "(checked by SmallCheck)"
+  [ SC.testProperty "sort == sort . reverse" $
+      \list -> sort (list :: [Int]) == sort (reverse list)
+  , SC.testProperty "Fermat's little theorem" $
+      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
+  -- the following property does not hold
+  , SC.testProperty "Fermat's last theorem" $
+      \x y z n ->
+        (n :: Integer) >= 3 SC.==> x^n + y^n /= (z^n :: Integer)
+  ]
+
+qcProps = testGroup "(checked by QuickCheck)"
+  [ QC.testProperty "sort == sort . reverse" $
+      \list -> sort (list :: [Int]) == sort (reverse list)
+  , QC.testProperty "Fermat's little theorem" $
+      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
+  -- the following property does not hold
+  , QC.testProperty "Fermat's last theorem" $
+      \x y z n ->
+        (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
+  ]
 
 unitTests = testGroup "Unit tests"
   [ testCase "List comparison (different length)" $
