a1c0a9409827d70087b752902029187de4cfe7a6
Danny Navarro <j@dannynavarro.net>
2014-11-11 11:28:42 +0100

    Always inline custom JavaScript
    It's small enough to not make it worth the hassle of including it
    externally.
    
    It's now loaded at the end of the document.
    
    See #14.

--- Test/Tasty/Runners/Html.hs
+++ Test/Tasty/Runners/Html.hs
@@ -197,34 +197,35 @@ generateHtml summary htmlPath mAssetsPath = do
             Nothing -> do H.style bootStrapCss
                           jQueryJs
                           bootStrapJs
-                          scriptJs
             Just (AssetsPath assetsPath) -> do
               H.link ! A.rel "stylesheet"
                      ! A.href (H.toValue $ assetsPath </> "bootstrap.min.css")
               forM_ ["bootstrap.min.js", "jquery-2.1.1.min.js", "bootstrap.min.js"] $ \str ->
                 H.script ! A.src (H.toValue $ assetsPath </> str ) $ mempty
 
-        H.body $ H.div ! A.class_ "container" $ do
-          H.h1 ! A.class_ "text-center" $ "Tasty Test Results"
-          H.div ! A.class_ "row" $
-            if summaryFailures summary > Sum 0
-              then
-                H.div ! A.class_ "alert alert-danger" $
-                  H.p ! A.class_ "lead text-center" $ do
-                    H.toMarkup . getSum $ summaryFailures summary
-                    " out of " :: Markup
-                    H.toMarkup tests
-                    " tests failed"
-              else
-                H.div ! A.class_ "alert alert-success" $
-                  H.p ! A.class_ "lead text-center" $ do
-                    "All " :: Markup
-                    H.toMarkup tests
-                    " tests passed"
+        H.body $ do
+          H.div ! A.class_ "container" $ do
+            H.h1 ! A.class_ "text-center" $ "Tasty Test Results"
+            H.div ! A.class_ "row" $
+              if summaryFailures summary > Sum 0
+                then
+                  H.div ! A.class_ "alert alert-danger" $
+                    H.p ! A.class_ "lead text-center" $ do
+                      H.toMarkup . getSum $ summaryFailures summary
+                      " out of " :: Markup
+                      H.toMarkup tests
+                      " tests failed"
+                else
+                  H.div ! A.class_ "alert alert-success" $
+                    H.p ! A.class_ "lead text-center" $ do
+                      "All " :: Markup
+                      H.toMarkup tests
+                      " tests passed"
 
-          H.div ! A.class_ "row" $
-            H.div ! A.class_ "well" $
-              H.toMarkup $ treeMarkup $ htmlRenderer summary
+            H.div ! A.class_ "row" $
+              H.div ! A.class_ "well" $
+                H.toMarkup $ treeMarkup $ htmlRenderer summary
+          scriptJs
 
  where
   -- Total number of tests
