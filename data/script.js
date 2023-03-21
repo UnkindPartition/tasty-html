65c0c7ed17080c21bf1183b944149a1b1fc0dd14
Owen Shepherd <owen@owen.cafe>
2023-03-21 19:50:05 +0100

    refactor: Self-calling function for js

--- data/script.js
+++ data/script.js
@@ -1,15 +1,17 @@
-const hasFailure = document.querySelector(".fail") !== null;
+(() => {
+  const hasFailure = document.querySelector(".fail") !== null;
 
-[...document.getElementsByClassName("group")].forEach(group => {
-  if (hasFailure && !group.classList.contains("fail")) {
-    group.classList.add("rollup");
-  }
+  [...document.getElementsByClassName("group")].forEach(group => {
+    if (hasFailure && !group.classList.contains("fail")) {
+      group.classList.add("rollup");
+    }
 
-  group.addEventListener("click", ev => {
-    group.classList.toggle("rollup");
+    group.addEventListener("click", ev => {
+      group.classList.toggle("rollup");
+    });
+    // prevent annoying highlighting when toggling group
+    group.addEventListener("mousedown", ev => {
+      if (event.detail > 1) { event.preventDefault(); }
+    }, false);
   });
-  // prevent annoying highlighting when toggling group
-  group.addEventListener("mousedown", ev => {
-    if (event.detail > 1) { event.preventDefault(); }
-  }, false);
-})
+})();
