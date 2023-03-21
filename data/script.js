025985305d987ded5fbc382d2488b2105396d419
Owen Shepherd <owen@owen.cafe>
2023-03-21 16:43:46 +0100

    feat: Roll up groups that contain no failures, if a failure exists somewhere

--- data/script.js
+++ data/script.js
@@ -1,9 +1,15 @@
+const hasFailure = document.querySelector(".fail") !== null;
+
 [...document.getElementsByClassName("group")].forEach(group => {
+  if (hasFailure && !group.classList.contains("fail")) {
+    group.classList.add("rollup");
+  }
+
   group.addEventListener("click", ev => {
     group.classList.toggle("rollup");
   });
   // prevent annoying highlighting when toggling group
-  group.addEventListener('mousedown', ev => {
+  group.addEventListener("mousedown", ev => {
     if (event.detail > 1) { event.preventDefault(); }
   }, false);
 })
